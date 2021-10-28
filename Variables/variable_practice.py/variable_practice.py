99f13070e992450d6bce52aba2398eceefdae5ee
s11334844 <70590336+s11334844@users.noreply.github.com>
2021-10-28 12:04:57 -0400

    [7B] v1.0

--- Variables/variable_practice.py/variable_practice.py
+++ Variables/variable_practice.py/variable_practice.py
@@ -1,1 +1,10 @@
-# Variable Practice, Elijah Burgess< 10/28/21 11:39am, version 0.0
+# Variable Practice, Elijah Burgess, 10/28/21 11:39am, version 0.0
+# Variable Types -- Most Common
+
+my_float = 3.32514
+my_int = 123455
+my_string = "I like games."
+
+print(my_float)
+print(my_int)
+print(my_string)
